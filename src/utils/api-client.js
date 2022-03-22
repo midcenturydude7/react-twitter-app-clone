3b83508541e465c30242f0f095dfe156d146695e
midCenturyDude7 <mgriffes@gmail.com>
2022-03-22 14:49:01 -0700

    Configured backend via MongoDB and set up Login/Logout functionality

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -6,11 +6,20 @@ export async function authenticate() {
     return await client.get("/auth/login").then((res) => res.data.user);
 }
 
-export async function login() {}
+export async function login(payload) {
+    await client.post("/auth/login", payload);
+    window.location.pathname = "/";
+}
 
-export async function signup() {}
+export async function signup(payload) {
+    await client.post("auth/signup", payload);
+    window.location.pathname = "/";
+}
 
-export async function logout() {}
+export async function logout() {
+    await client.get("/auth/logout");
+    window.location.pathname = "/";
+}
 
 export async function getNotifications() {}
 
