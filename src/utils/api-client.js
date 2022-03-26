d4f510ff503f97bf572b0956baf0fea74abb222a
midCenturyDude7 <mgriffes@gmail.com>
2022-03-25 21:13:16 -0700

    Completed PostsReposts component

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -41,7 +41,8 @@ export async function getPosts() {
 }
 
 export async function getPostLikes(postId) {
-    return await client.get(`/api/post/${postId}/likes`)
+    return await client
+        .get(`/api/post/${postId}/likes`)
         .then(res => res.data.users);
 }
 
@@ -49,7 +50,11 @@ export async function followUser() {}
 
 export async function unfollowUser() {}
 
-export async function getPostReposts() {}
+export async function getPostReposts(postId) {
+    return await client
+        .get(`/api/post/${postId}/reposts`)
+        .then(res => res.data.users);
+}
 
 export async function getUserFollowers() {}
 
